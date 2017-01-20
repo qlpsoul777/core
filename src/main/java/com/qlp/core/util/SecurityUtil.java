57e6c1d3d560ff583a8f412dcfdfbff63953a4bb
qlp <qlp@cpyf-3-PC>
2017-01-20 17:41:22 +0800

    shiro工具类

--- src/main/java/com/qlp/core/util/SecurityUtil.java
+++ src/main/java/com/qlp/core/util/SecurityUtil.java
@@ -3,8 +3,12 @@ package com.qlp.core.util;
 import org.apache.shiro.SecurityUtils;
 
 public class SecurityUtil {
-	
-	 public static String getCurrentUserLoginName() {
-	        return (String) SecurityUtils.getSubject().getPrincipal();
-	    }
+
+	public static String getCurrentUserLoginName() {
+		return (String) SecurityUtils.getSubject().getPrincipal();
+	}
+
+	public static void logout() {
+		SecurityUtils.getSubject().logout();
+	}
 }
